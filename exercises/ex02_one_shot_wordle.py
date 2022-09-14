2b76d9ff97ef6cbd81eb1133765433d1af9c291a
Jewels Fernandez <jewelsa@ad.unc.edu>
2022-09-14 16:22:00 -0400

    Progress on Exercise 3

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,7 +3,7 @@
 __author__ = "730598599"
 
 
-#Below are most of my variable declarations.
+# Below are most of my variable declarations.
 secret_wordle: str = "python"
 number: int = len(secret_wordle)
 WHITE_BOX: str = "\U00002B1C"
@@ -12,39 +12,39 @@ YELLOW_BOX: str = "\U0001F7E8"
 counter: int = 0
 emoji: str = ""
 
-#This is where the user would input their guessed word, which will be established under the variable "guess."
+# This is where the user would input their guessed word, which will be established under the variable "guess."
 guess: str = input(f"What is your {number}-letter guess? ")
 
-#If the user enters an incorrect amount of letters, this line will prompt them to retry.
+# If the user enters an incorrect amount of letters, this line will prompt them to retry.
 while len(guess) != number:
     guess = input(f"That was not {number} letters! Try again: ")
 
-#This is where the program will begin to identify letter matches if the guess doesn't match the secret word.
+# This is where the program will begin to identify letter matches if the guess doesn't match the secret word.
 if guess != secret_wordle:
     while counter < len(guess):
-        #Below is a code that will establish whether a letter is in the correct area;
+        # Below is a code that will establish whether a letter is in the correct area;
         if guess[counter] == secret_wordle[counter]:
             emoji += GREEN_BOX
-            #if not, the else statement will begin the process of seeing whether a yellow or white box is needed.
+            # if not, the else statement will begin the process of seeing whether a yellow or white box is needed.
         else:
             alt_count: int = 0
             locate: bool = False
-            #While the alternate counter is less than the current index of the secret word, it will run through the indexes to see if a white or yellow square should be put down in the current emoji index.
+            # While the alternate counter is less than the current index of the secret word, it will run through the indexes to see if a white or yellow square should be put down in the current emoji index.
             while alt_count < len(secret_wordle):
                 if guess[counter] == secret_wordle[alt_count]:
                     locate = True
                 alt_count += 1
-                #If the boolean expression is True, meaning the character exists in the secret word, a yellow square is put down;
-            if locate == True:
+                # If the boolean expression is True, meaning the character exists in the secret word, a yellow square is put down;
+            if locate is True:
                 emoji += YELLOW_BOX
-                #if not, a white square is put down.
+                # if not, a white square is put down.
             else:
                 emoji += WHITE_BOX
         counter += 1
-        #After all that, the emoji string is is printed and the user is told that they guessed incorrectly.
+        # After all that, the emoji string is is printed and the user is told that they guessed incorrectly.
     print(emoji)
     print("Not quite. Play again soon!")
-    #if the user guessed correctly, then the correct amount of green square emojis are declared and printed, and the player is congratulated.
+    # if the user guessed correctly, then the correct amount of green square emojis are declared and printed, and the player is congratulated.
 else:
     while counter < len(guess):
         emoji += GREEN_BOX
---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,77 @@
+"""EX03 - Structured Wordle."""
+
+__author__ = "730598599"
+
+# This defined function searches to see if a specific character is found in a specific string.
+def contains_char(string_searched: str, search: str) -> bool:
+    """Generates a bool value that determines if a character is found in a string."""
+    # This assertion makes sure that the function will only search for one specific character.
+    assert len(search) == 1
+    counter: int = 0
+    while counter < len(string_searched):
+        # If a character is found in a string, the function will return True. If not, the counter will increase until the loop ends and it will return False.
+        if search == string_searched[counter]:
+            return True
+        else:
+            counter += 1
+    return False
+
+# This function will return a string of emojis based on whether the characters of the "guess" string are in the correct spot, incorrect spot, or not present compared to the "secret" string.
+def emojified(guess: str, secret: str) -> str:
+    """Generates the necessary emojis based on the user's guess."""
+    # This assertion makes sure that the lengths of the "guess" and "secret" strings are the same.
+    assert len(guess) == len(secret)
+    # These next three variables correspond to codes for the necessary emojis.
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+    count_two: int = 0
+    emoji: str = ""
+    # In this loop, while the conter equals the length of the "secret" string, the program will determine whether a green, yellow, or white square emoji is needed.
+    while count_two < len(secret):
+        if guess[count_two] == secret[count_two]:
+            emoji += GREEN_BOX
+        elif contains_char(secret, guess[count_two]) is True:
+            emoji += YELLOW_BOX
+        else:
+            emoji += WHITE_BOX
+        count_two += 1
+    # Once the loop is completed, the function will return the string of emojis it created.
+    return emoji
+
+# This defined function establishes that a string must be a certain length in order to be returned.
+def input_guess(word_len: int) -> str:
+    """Establishes the length of the word needing to be guessed."""
+    user_word: str = input(f"Enter a {word_len} character word: ")
+    while len(user_word) != word_len:
+        user_word = input(f"That wasn't {word_len} chars! Try again: ")
+    return user_word
+
+# This defined function establishes what the secret word is, as well as pulls together the previous functions in order to make a full-fledged game.
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    # These variables establish the word needing to be guessed, the turn number the player is on, and whether the player has won or not.
+    secret_wordle: str = "codes"
+    user_turns: int = 1
+    win: bool = False
+    # This while loop establishes that the player only has six turns to play while also ensuring that the game ends if they guess correctly.
+    while user_turns <= 6 and win is False:
+        # The code below tells the player what turn they are on.
+        print(f"=== Turn {user_turns}/6 ===")
+        # The two lines of code below call the "input_guess" and "emojified" functions in order to use them in the "main" function.
+        num_guess: str = input_guess(len(secret_wordle))
+        print(emojified(num_guess, secret_wordle))
+        # If the guessed word matches the secret word, a message saying the player won is printed and the function is completed.
+        if num_guess == secret_wordle:
+            print(f"You won in {user_turns}/6 turns!")
+            win = True
+            # If the guessed word does not match the secret word, the turn number is increased and the loop is restarted.
+        else:
+            user_turns += 1
+        # If the player has played six turns and still has not guessed correctly, a message saying they lost is printed and the function is completed.
+        if user_turns > 6:
+            print("X/6 - Sorry, try again tomorrow!")
+
+# These last lines of code make it so that the "main" function can be played as a module, as well as making it so that it can be imported in other modules.
+if __name__ == "__main__":
+    main()
